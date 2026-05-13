2523f1fdf4d496edd80300b016d9a5093a3f718d
Anilcodes01 <anilcodes01@gmail.com>
2026-05-13 14:35:58 +0530

    Initial Commit

---
+++ Rocky/RockyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockyApp.swift
+//  Rocky
+//
+//  Created by Anil Pal on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct RockyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
